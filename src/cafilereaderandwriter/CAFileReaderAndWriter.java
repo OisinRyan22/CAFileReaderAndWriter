61f951e7fc568ead274de90d4d1b85cab00ade1a
OisinRyan22 <sba23149@student.cct.ie>
2024-01-27 18:11:05 +0000

    Validation process
    Start of the validating for the input file

--- src/cafilereaderandwriter/CAFileReaderAndWriter.java
+++ src/cafilereaderandwriter/CAFileReaderAndWriter.java
@@ -21,34 +21,46 @@ public class CAFileReaderAndWriter {
     //https://github.com/OisinRyan22/CAFileReaderAndWriter
     //Input FilePath("C:\\Users\\ocean\\OneDrive\\Documents\\NetBeansProjects\\students.txt")
     //Output FilePath ("status.txt")
-    
     public static void main(String[] args) throws IOException {
-        try
-            (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\ocean\\OneDrive\\Documents\\NetBeansProjects\\students.txt"))) {
-           
-            String line; 
+        try ( BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\ocean\\OneDrive\\Documents\\NetBeansProjects\\students.txt"))) {
+
+            String line;
             while ((line = br.readLine()) != null) {
                 String firstName = line.trim();
                 String numofClasses = br.readLine().trim();
                 String studentNum = br.readLine().trim();
-                
+
                 String[] names = firstName.split(" ", 2);           //Splits first and second name on first line 
                 firstName = names[0];
                 String secondName = (names.length > 1) ? names[1] : "";
-                
-                String error = validateData (firstName, secondName, numofClasses, studentNum);      //Validates data
-                
+
+                String error = validateData(firstName, secondName, numofClasses, studentNum);      //Validates data
+
                 if (error != null) {
-                System.out.println("Data is invalid for" + firstName + " " + secondName + ":");      
-                System.out.println(error);
-                //If validation fails, this output and error message        
+                    System.out.println("Data is invalid for" + firstName + " " + secondName + ":");
+                    System.out.println(error);
+                    //If validation fails, this output and error message        
+                }
+
             }
-          
-            
+        }
     }
-    } 
-}
 
     private static String validateData(String firstName, String secondName, String numofClasses, String studentNum) {
-     
+        
+        if (!validName(firstName)) {
+            return "Error in first name! Must contain letters only!";       //Validates the first name 
+        }
+        if (!validName(secondName)) {
+            return "Error in second name! Must contain letters and/or numbers only!";   //Validates the second name 
+        }
+        if (!validClasses(numofClasses)) {
+            return "Error in number of classes entered! Must be between 1 and 8!";     //Validates the number of classes
+        }
+        if (!validstudentNum(studentNum)) {
+            return " Error, invalid student number! Formet 00AAA000....";     //Validates the student number
+        }
+        
+    }
+
     }
